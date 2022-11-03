214525858fa19af0fb22292584400b9226a2a38a
Vygnich Oleksandr <vt211_vos@student.ztu.edu.ua>
2022-11-03 08:53:01 +0200

    Lab7

--- lab7/person.py
+++ lab7/person.py
@@ -11,3 +11,18 @@ class Person:
         return str(int(age))
     def get_fullname(self):
         return f"{self.first_name} {self.surname}"
+    def modifier(filename):
+        with open('filename', 'r', encoding='utf8') as file:
+            data = file.readlines()
+        info = [i.replace("\n", "") for i in data]
+        person = Person(info[0], info[1], info[2], datetime.datetime.strptime(info[3], "%Y-%m-%d"))
+        print(person.get_age())
+
+        with open(filename, 'w', encoding='utf8') as file:
+            file.write("")
+        with open(filename, 'a', encoding='utf8') as file:
+            for i in range(len(data)):
+                if i == 2:
+                    file.write(person.get_fullname() + '\n')
+                file.write(data[i])
+            file.write("\n" + person.get_age())
